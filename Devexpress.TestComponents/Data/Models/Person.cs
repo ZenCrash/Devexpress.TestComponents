b844f856cb758fac1e929748229cbef2ef285af9
Allan <76095439+ZenCrash@users.noreply.github.com>
2025-01-10 00:37:44 +0100

    Add project files.

---
+++ Devexpress.TestComponents/Data/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace Devexpress.TestComponents.Data.Models
+{
+  public class Person
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Age { get; set; }
+    public bool isMarried { get; set; }
+    public DateTime? WakesUpAt { get; set; }
+    public DateTime? GoesToBedAt { get; set; }
+  }
+}
---
+++ Devexpress.TestComponents/Data/Validation/PersonValidator.cs
@@ -0,0 +1,27 @@
+﻿using Devexpress.TestComponents.Data.Models;
+using FluentValidation;
+
+namespace Devexpress.TestComponents.Data.Validation
+{
+  public class PersonValidator : AbstractValidator<Person>
+  {
+    public PersonValidator()
+    {
+      RuleFor(x => x.isMarried)
+        .NotNull();
+        ;
+      RuleFor(x => x.GoesToBedAt)
+        .Must((x , field) => DateTimeGreaterThenCheck(x.WakesUpAt, x.GoesToBedAt))
+        ;
+    }
+
+    public bool DateTimeGreaterThenCheck(DateTime? startDate, DateTime? endDate)
+    {
+      if (startDate != null && endDate != null && startDate < endDate)
+      {
+        return true;
+      }
+      return true;
+    }
+  }
+}
